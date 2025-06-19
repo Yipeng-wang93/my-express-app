527d5616d3e0421d9f4fc78fb0fcbb58f6dd6beb
Yipeng Wang <n01625427@humber.ca>
2025-06-19 16:47:20 -0400

    fix: update message and remove listen for vercel

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World! My name is Yipeng Wang. This is my first app!');
 });
- 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+
+module.exports = app;
+// app.listen(port, () => {
+//   console.log(`App listening on port ${port}`);
+// });
